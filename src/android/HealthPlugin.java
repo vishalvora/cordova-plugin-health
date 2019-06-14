194990a94f1849603f36c30a5ad9f740405eaff3
vishalvora <vishalgvora@gmail.com>
2019-06-15 02:34:04 +0530

    Update HealthPlugin.java

--- src/android/HealthPlugin.java
+++ src/android/HealthPlugin.java
@@ -427,7 +427,7 @@ public class HealthPlugin extends CordovaPlugin {
 
     
      public void subscribe() {
-        Fitness.getRecordingClient(this, GoogleSignIn.getLastSignedInAccount())
+        Fitness.getRecordingClient(this, GoogleSignIn.getLastSignedInAccount(this))
                 .subscribe(DataType.AGGREGATE_STEP_COUNT_DELTA)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
